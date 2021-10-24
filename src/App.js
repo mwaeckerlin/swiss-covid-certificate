0896afb6dca5f0e13c586979bfb30f04a8dfe46b
Marc Wäckerlin <marc@mrw.dev>
2021-10-24 19:23:40 +0200

    formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-//import './App.styl'
-import QRCode from "react-qr-code"
+import './App.css'
+import QRCode from "@pacta-app/qrcode-react"
 
 const App = () => {
   const useStickyState = (defaultValue, key) => {
@@ -16,7 +16,7 @@ const App = () => {
     return [value, setValue];
   }
   const [edit, setEdit] = useStickyState(false, 'edit')
-  const [name, setName] = useStickyState('Peter Muster', 'name')
+  const [name, setName] = useStickyState('Muster Peter (ändern)', 'name')
   const [birthday, setBirthday] = useStickyState("1970-01-01", 'birthday')
   const format = date => date.toLocaleDateString('de-CH', { day: '2-digit', month: '2-digit', year: 'numeric' })
   const [date1, date2, date3] = [new Date(), new Date(), new Date()]
@@ -27,14 +27,13 @@ const App = () => {
   date3.setDate(date3.getDate() + 27)
   return (
     <main onClick={() => setEdit(false)} onKeyDown={e => { if (e.key === "Enter") setEdit(false) }}>
+      <header>
+        COVID-ZERTIFIKAT
+      </header>
       <div class="cert">
-        <div class="header">
-          COVID ZERTIFIKAT
-        </div>
         <QRCode
-          value="Sind alle impfwilligen erwachsenen Personen vollständig geimpft, beginnt die Normalisierungsphase. Der Bundesrat ist der Ansicht, dass dann keine starken gesellschaftlichen und wirtschaftlichen Einschränkungen mehr zu rechtfertigen sind. Die verbleibenden Massnahmen (Zugangs- und Kapazitätsbeschränkungen) sollen schrittweise aufgehoben werden. An dieser Strategie soll auch dann festgehalten werden, wenn die Impfbereitschaft der Bevölkerung entgegen der Erwartungen tief bleibt.
-
-          Auch nach der Impfung aller impfbereiten Personen wird aber das Virus weiter zirkulieren. Es ist davon auszugehen, dass sich langfristig nicht-geimpfte und nicht-genesene Personen anstecken werden. Je grösser der Anteil dieser Personen ist, desto wahrscheinlicher dürften mögliche Ausbrüche sein, und desto höher wird auch die Zahl der schweren Krankheitsverläufe und Todesfälle ausfallen. Mit der kostenlosen Impfung steht eine hoch wirksame Möglichkeit zur Verfügung, sich individuell vor einer Ansteckung und einer Erkrankung zu schützen. Diese Tatsache wird der Bundesrat bei seinen zukünftigen Entscheiden berücksichtigen und gleichzeitig den persönlichen Impfentscheid jeder und jedes einzelnen respektieren. Um eine erneute Krankheitswelle und eine Überlastung des Gesundheitssystems zu verhindern, ist es aber von grösster Bedeutung, dass sich möglichst viele Menschen in der Schweiz impfen lassen"
+          size={256}
+          value="Sind alle impfwilligen erwachsenen Personen vollständig geimpft, beginnt die Normalisierungsphase. Der Bundesrat ist der Ansicht, dass dann keine starken gesellschaftlichen und wirtschaftlichen Einschränkungen mehr zu rechtfertigen sind. Die verbleibenden Massnahmen (Zugangs- und Kapazitätsbeschränkungen) sollen schrittweise aufgehoben werden. An dieser Strategie soll auch dann festgehalten werden, wenn die Impfbereitschaft der Bevölkerung entgegen der Erwartungen tief bleibt."
         />
       </div>
       <div onClick={e => { e.stopPropagation(); setEdit(true) }}>
@@ -50,8 +49,13 @@ const App = () => {
         </div>
       </div>
       <div class="info">Nur mit einem Ausweisdokument gültig</div>
-      <div class="Gültigkeit">
-        <div>Gültigkeit in der Schweiz</div><div>bis <div class="fat">{format(date1)}</div></div>
+      <div class="info">
+        <table>
+          <tr>
+            <td>Gültigkeit in der Schweiz</td>
+            <td>bis <div class="fat">{format(date1)}</div></td>
+          </tr>
+        </table>
       </div>
       <div class="data">
         <div>IMPFUNG</div>
@@ -69,16 +73,21 @@ const App = () => {
         <div>Biontech Manufactoring GmbH</div>
         <div>Hersteller</div>
         <div>Manufacturer</div>
+      </div>
+      <div class="data">
         <div>{format(date2)}</div>
         <div>Impfdatum</div>
         <div>Date of Vaccination</div>
         <div>Schweiz / Switzerland</div>
         <div>Land der Impfung</div>
         <div>Country of Vaccination</div>
+      </div>
+      <div class="data">
         <div>Bundesamt für Gesundheit (BAG)</div>
         <div>Herausgeber</div>
         <div>Issuer</div>
         <div>urn:uvci:01:CH:4F39ED278D2382A768C746DB</div>
+        <div />
         <div>UVCI</div>
       </div>
       <div class="issuing-date">
@@ -86,13 +95,13 @@ const App = () => {
         <div>Certificate generated on {format(date3)}, 10:42</div>
         <div>Date format used: dd:mm:yyyy</div>
       </div>
-      <div class="info">
+      <div class="info left">
         <div>Dieses Zertifikat ist kein Reisedokument.</div>
         <div>Die wissenschaftlichen Erkenntnisse über Covid-19-Impfungen und -Tests sowie über die Genesung von einer Covid-19-Infektion entwickeln sich ständig weiter, auch im Hinblick auf neue besorgniserregende Virusvarianten.</div>
         <div>Bitte informieren Sie sich vor der Reise über die am Zielort geltenden Gesundheitsmassnahmen und damit verbundenen Beschränkungen.</div>
       </div>
       <div class="menuitem">Zertifikat Light</div>
       <div class="menuitem">Exportieren</div>
-    </main>)
+    </main >)
 }
 export default App;
